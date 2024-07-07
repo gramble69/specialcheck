eb63c77b20eeaccc5d79e3d8509f6f6b1fe32460
gramble69 <119713260+gramble69@users.noreply.github.com>
2024-07-07 08:37:51 -0500

    formated it

--- checker.c
+++ checker.c
@@ -1,36 +1,39 @@
-#include<stdio.h>
-int check_prime(int a){
-    for(int b = 2; 1; b++){
-        if(b==a){
-            printf("%d is prime",a);
+#include <stdio.h>
+int check_prime(int a)
+{
+    for (int b = 2; 1; b++)
+    {
+        if (b == a)
+        {
+            printf("%d is prime", a);
             return 1;
         }
-        if(a%b==0){
-            printf("%d is composite",a);
+        if (a % b == 0)
+        {
+            printf("%d is composite", a);
             return 0;
-            }
+        }
     }
-    
 }
 
 int prime_list(int a)
 {
     const int max = 999999;
-    char not_prime[max]={};
-    for(int i = 2; i<max; i++)
+    char not_prime[max] = {};
+    for (int i = 2; i < max; i++)
     {
         if (not_prime[i])
         {
             continue;
         }
-        printf("%d is prime\n",i);
+        printf("%d is prime\n", i);
         if (!--a)
         {
             return i;
         }
-        for(int b = i+i; b<max; b+=i)
+        for (int b = i + i; b < max; b += i)
         {
-            not_prime[b]=1;
+            not_prime[b] = 1;
         }
     }
     return 0;
@@ -56,24 +59,24 @@ int prime_list_sum(int a)
 {
     int sum = 0;
     const int max = 999999;
-    char not_prime[max]={};
-    for(int i = 2; i<max; i++)
+    char not_prime[max] = {};
+    for (int i = 2; i < max; i++)
     {
         if (not_prime[i])
         {
             continue;
         }
         sum = sum + i;
-        printf("%d is prime, added to sum\n ",i);
+        printf("%d is prime, added to sum\n ", i);
         if (!--a)
         {
             return i;
         }
-        for(int b = i+i; b<max; b+=i)
+        for (int b = i + i; b < max; b += i)
         {
-            not_prime[b]=1;
+            not_prime[b] = 1;
         }
     }
-    printf("%d is the sum",sum);
+    printf("%d is the sum", sum);
     return 0;
 }
